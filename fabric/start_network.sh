518caa6396fd1da795700d551edde27aa18798b2
stefko3z <svladov.dev@gmail.com>
2020-05-26 00:51:48 +0200

    update automation scripts

--- fabric/start_network.sh
+++ fabric/start_network.sh
@@ -1,36 +1,32 @@
-
+#!/bin/bash
 # USAGE EXAMPLE: bash start_network.sh ./artifacts/src/github.com/fabcar/javascript fabcar
 # Then end: bash end_network.sh
-
 # NOTE: make sure that the Hyperledger Fabric binaries are in the PATH env var
 
+# Step 1: Create crypto material
+pushd ./artifacts/channel/
+sh create-artifacts.sh
+popd
 
-# Create crypto material
-cd ./artifacts/channel/
-bash create-artifacts.sh
-cd ../
-
-# Start docker compose
-# NOTE: difference in versions, sometimes needs: docker compose up -d
+# Step 2: Start docker compose in detached mode
+# NOTE: difference in versions
+pushd ./artifacts
 docker-compose up -d
-
-# Create channel
-sleep 20
-cd ../
-bash ./createChannel.sh
-
-# Wait and check that all peers have joined, assuming for now 10 seconds
-sleep 30
-
-echo "=========================================== CHECKING THAT ALL PEERS HAVE JOINED =========================="
+popd
+# Wait 15s to make sure all containers are up and running
+sleep 15s
 docker ps
-echo "=========================================== END CHECKING THAT ALL PEERS HAVE JOINED =========================="
-# TODO: some better checks here
 
+# Step 3: Create channel
+sh ./createChannel.sh
 
-# Deploy chaincode
-# NOTE: check if rights are correctly set
 sleep 10
 
+# Step 4: Check that channels are joined
+docker exec -it peer0.org1.example.com sh -c "peer channel list"
+docker exec -it peer0.org2.example.com sh -c "peer channel list"
+docker exec -it peer0.org3.example.com sh -c "peer channel list" 
+
+# Step 5: Deploy chaincode
 ./deployChaincode.sh $1 $2
 
