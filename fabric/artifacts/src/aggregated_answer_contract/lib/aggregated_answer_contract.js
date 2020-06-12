8bd28714c4d41c3c45850964eb423bbc39db888d
nikita68 <n.makarov@hotmail.com>
2020-06-12 11:18:31 +0200

    Agg: calling query

--- fabric/artifacts/src/aggregated_answer_contract/lib/aggregated_answer_contract.js
+++ fabric/artifacts/src/aggregated_answer_contract/lib/aggregated_answer_contract.js
@@ -7,8 +7,10 @@ const ClientIdentity = require('fabric-shim').ClientIdentity;
 const num_majority = 1;
 const mo_id = "org1";
 const dc_id = "org2";
-const oo_id = "org3"; 
-//const oo_id = "org1"; // UNCOMMENT FOR TESTING IF BUGS ARISE WITH ORG3
+//const oo_id = "org3"; // TODO: COMMENT BACK OUT!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const oo_id = "org1"; // UNCOMMENT FOR TESTING IF BUGS ARISE WITH ORG3
+const cc_query_name = "query_contract";
+const channel_name = "mychannel";
 
 
 class AggAnswerContract extends Contract {
@@ -49,35 +51,47 @@ class AggAnswerContract extends Contract {
         return answerAsBytes.toString();
     }
 
-
-
     async createAnswer(ctx, encr_answer_text, dc_wallet, query_id, user_wallet_list) {
         console.info('============= START : Create Answer ===========');
-        
-        // convert use_wallet_list appropriatly
-        user_wallet_list = JSON.parse(user_wallet_list);  // TODO: check if appropriate
-        
-        const rID1 = await ctx.stub.getState('counter');
-        let rID = rID1.toString();
-        await ctx.stub.putState('counter', Buffer.from((parseInt(rID) + 1).toString()));  // Increase counter for uniqueness
+        let cid = new ClientIdentity(ctx.stub);
 
-        let answer_id = 'a' + rID;
 
-        const answer = {
-            answer_id: answer_id,
-            query_id: query_id, 
-            dc_wallet: dc_wallet,
-            encr_answer_text: encr_answer_text,
-            user_wallet_list: user_wallet_list,
-        };
+        if(cid.assertAttributeValue('hf.Affiliation', oo_id + '.department1')){
 
-        // TODO: automatically transfer funds via Coin contract
+            // convert use_wallet_list appropriatly
+            user_wallet_list = JSON.parse(user_wallet_list);  // TODO: check if appropriate
+            
+            const rID1 = await ctx.stub.getState('counter');
+            let rID = rID1.toString();
+            await ctx.stub.putState('counter', Buffer.from((parseInt(rID) + 1).toString()));  // Increase counter for uniqueness
 
-        // TODO: set stage for query
-        
-        await ctx.stub.putState(answer_id, Buffer.from(JSON.stringify(answer)));
-        console.info('============= END : Create Answer ===========');
-        return answer_id;
+            let answer_id = 'a' + rID;
+
+            const answer = {
+                answer_id: answer_id,
+                query_id: query_id, 
+                dc_wallet: dc_wallet,
+                encr_answer_text: encr_answer_text,
+                user_wallet_list: user_wallet_list,
+            };
+
+            // TODO: automatically transfer funds via Coin contract
+            // TODO: find out which CC to call
+
+
+            // set served stage (nr 5) for query
+            // TODO: check if works
+            const args = ["setQueryStage", query_id, 5];
+            await ctx.stub.invokeChaincode(cc_query_name, args, channel_name);
+
+            await ctx.stub.putState(answer_id, Buffer.from(JSON.stringify(answer)));
+            console.info('============= END : Create Answer ===========');
+            return answer_id;
+
+        } else {
+            // error if not OO
+            throw new Error('Not Oversight Organization credentials!');
+        }
     }
 
     
