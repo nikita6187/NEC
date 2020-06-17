a91b43a2480e1e6193a278c7323144bb92eaef5a
nikita68 <n.makarov@hotmail.com>
2020-06-17 14:01:42 +0200

    Fabric API folder restructuring 2

---
+++ docs_fabric_api/query/README.md
@@ -0,0 +1,38 @@
+# Fabric API for Query contract
+
+```
+cd NEC/fabric/
+```
+
+Optionally:
+```
+bash ./setup_network.sh
+```
+
+To init network:
+```
+bash start_network.sh
+bash ./deployChaincode.sh ./artifacts/src/query_contract/ query_contract 1 initLedger []
+
+```
+
+Optionally:
+```
+python3 generate_certificates.py 1
+python3 generate_certificates.py 2
+python3 generate_certificates.py 3
+```
+
+Then:
+```
+cd fabric/api-2.0
+npm run start 4000 clean
+```
+
+Then in new terminal window.
+```
+python3 quicktest_api.py 4000 nikita3 2 post query_contract createQuery query_test_text 1 40 wallet3
+python3 quicktest_api.py 4000 nikita20 2 req query_contract getQuery q1
+python3 quicktest_api.py 4000 nikita27j 3 req query_contract approveQuery 1 (doesn't work yet due to org3.department1 bug)
+python3 quicktest_api.py 4000 nikita899 1 post query_contract setQueryStage q1 1
+```
---
+++ docs_fabric_api/query/old/queryTest.js
@@ -0,0 +1,79 @@
+
+const { Gateway, Wallets } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+const YAML = require('yaml');
+
+
+async function main() {
+    try {
+        
+        // Get airline id
+        let org_id = process.argv[2];
+        console.log('Loading up for org id: ' + org_id);
+
+        // get op: getQuery, createQuery, approveQuery, setQueryStage (case sensitive)
+        let op = process.argv[3];
+
+
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', 'fabric', 'api-2.0', 'config', 'connection-org' + org_id + '.yaml');
+        let ccp = YAML.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet'+org_id);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('query_contract');
+
+        // Submit the specified transaction.
+        if(op === 'createQuery'){
+            const result = await contract.submitTransaction('createQuery', process.argv[4], process.argv[5], process.argv[6], process.argv[7]);
+            console.log("New query id: ");
+            console.log(`${result.toString()}`);
+        }
+
+        if(op === 'setQueryStage'){
+            const result = await contract.submitTransaction('setQueryStage', process.argv[4], process.argv[5]);
+            console.log("All flight data: ");
+            console.log(`${result.toString()}`);
+        }
+
+        if(op === 'getQuery'){
+            const result = await contract.evaluateTransaction('getQuery', process.argv[4]);
+            console.log("Specified flight data: ");
+            console.log(`${result.toString()}`);
+        }
+
+        if(op === 'approveQuery'){
+            const result = await contract.submitTransaction('approveQuery', process.argv[4], process.argv[5]);
+            console.log("Booking complete");
+            console.log(`${result.toString()}`);
+        }
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
