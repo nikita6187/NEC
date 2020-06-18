1527e69b0b0f1d9004f8d2860740e4577ecfc88a
nikita68 <n.makarov@hotmail.com>
2020-06-18 08:59:16 +0200

    CT: better helper functions

--- clients/template/ClientTemplate.py
+++ clients/template/ClientTemplate.py
@@ -21,14 +21,21 @@ addr_agg = "localhost:11900"
 # HF connection config
 addr_hf_api = "localhost:4000"
 hf_token = None
-org_id = 1  # TODO: change this appropriatly
+org_id = str(1)  # TODO: change this appropriatly
 # See /fabric/api-2.0/quicktest_api.py for example how to contact HF API via python
 
 
 # Helper code
 pool = Pool(10)
 
-
+"""
+Function to call a HTTP request async and only printing the result.
+to_get: Bool, True = GET, False = POST
+url: string URL to call
+data: list/dict for POST data
+headers: list/dict of headers
+params: list/dict for GET params
+"""
 def fire_and_forget(to_get, url, data=[], headers=[], params=[]):
     # Example: fire_and_forget(True, "https://www.google.com/")
 
@@ -49,7 +56,12 @@ def fire_and_forget(to_get, url, data=[], headers=[], params=[]):
                          callback=on_success, error_callback=on_error)
 
 
+"""
+Returns a HF API token needed for communication.
+return: string of token
+"""
 def register_hf_api_token():
+    # NOTE: not tested fully
     url_users = addr_hf_api + "/users"
     org_str = "Org" + str(org_id)
     username = "default_user"
@@ -58,6 +70,55 @@ def register_hf_api_token():
     token = r.json()['token']
     r.close()
     return token
+
+
+"""
+Function to invoke (= HTTP POST) something from a chaincode via the HF API.
+token: token which is returned by register_hf_api_token
+chaincode_name: name of chaincode
+function: function name
+args: list of string args
+returns: json of response
+"""
+def hf_invoke(token, chaincode_name, function, args):
+    # NOTE: not tested fully
+    url_post = addr_hf_api + "/channels/mychannel/chaincodes/" + chaincode_name
+    body = {"fcn": function,
+            "peers": "peer0.org" + org_id + ".example.com",
+            "chaincodeName": chaincode_name,
+            "channelName": "myChannel",
+            "args": args}
+    headers = {"Authorization": "Bearer " + token}
+    r = requests.post(url_post, data=body, headers=headers)
+    return r.json()
+
+
+"""
+Function to get something from a chaincode via the HF API.
+token: token which is returned by register_hf_api_token
+chaincode_name: name of chaincode
+function: function name
+args: list of string args
+returns: json of response
+"""
+def hf_get(token, chaincode_name, function, args):
+    # NOTE: not tested fully
+    headers = {"Authorization": "Bearer " + token}
+    params = {"args": args,
+              "peer": "peer0.org" + org_id + ".example.com",
+              "fcn": function}
+    url_req = addr_hf_api + "/channels/mychannel/chaincodes/" + chaincode_name
+    r = requests.get(url_req, headers=headers, params=params)
+    return r.json()
+
+
+"""
+Helper function to invoke async operations i.e. you call the function but do not wait for the response.
+function: reference to function to call
+args: tuple of args to pass to function
+"""
+def invoke_async_function(function, args):
+    pool.apply_async(function, args)
 
 
 # Logic class
